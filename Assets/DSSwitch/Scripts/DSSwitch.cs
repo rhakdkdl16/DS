ec25c6fe14fd3788146011bc4a3896e11979ac40
dasung jung <54258507+zhfvkq77@users.noreply.github.com>
2019-10-10 16:17:37 +0900

    스프라이트 추가
    -스위치 스프라이트 추가함.
    -씬에 스위치형태 만들어음.

---
+++ Assets/DSSwitch/Scripts/DSSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DSSwitch : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
